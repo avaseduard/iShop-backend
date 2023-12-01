db27ba99c7d133e459ebd8c00b59c82d0c99eeba
eduard <avas.eduard@gmail.com>
2023-12-01 19:07:22 +0200

    modified the filter function to work with combined filters

--- controllers/product.js
+++ controllers/product.js
@@ -100,7 +100,7 @@ exports.productsCount = async (req, res) => {
   res.json(total)
 }
 
-//
+// Product rating functionality
 exports.productStar = async (req, res) => {
   const product = await Product.findById(req.params.productId).exec()
   const user = await User.findOne({ email: req.user.email }).exec()
@@ -141,6 +141,7 @@ exports.productStar = async (req, res) => {
   }
 }
 
+// Product page list related product functionality
 exports.listRelated = async (req, res) => {
   // Find product based on id sent from front end
   const product = await Product.findById(req.params.productId).exec()
@@ -158,166 +159,103 @@ exports.listRelated = async (req, res) => {
   res.json(related)
 }
 
-// Search & filter
+// Search & filter //
 
-// Query the db based on the text property (which is true in the model, for title and description)
-const handleQuery = async (req, res, query) => {
-  // const products = await Product.find({ $text: { $search: query } })
-  const products = await Product.find({
-    title: {
-      $regex: query,
-      $options: 'i',
-    },
-    description: {
-      $regex: query,
-      $options: 'i',
-    },
-  })
-    .populate('category', '_id name')
-    .populate('subcategories', '_id name')
-    .populate('postedBy', '_id name')
-    .exec()
-
-  res.json(products)
+// Product populate functionality
+const applyPopulations = async products => {
+  const productIds = products.map(product => product._id)
+  try {
+    const populatedProducts = await Product.find({ _id: { $in: productIds } })
+      .populate('category', '_id name')
+      .populate('subcategories', '_id name')
+      .populate('postedBy', '_id name')
+      .exec()
+    return populatedProducts
+  } catch (error) {
+    throw error
+  }
 }
 
-// Query the db based on price array that we get from fe (ie [10, 100])
-const handlePrice = async (req, res, price) => {
-  try {
-    const products = await Product.find({
+// Combined filter search
+exports.searchFilters = async (req, res) => {
+  // Destructure filters from req
+  const { query, price, category, stars, subcategory, shipping, color, brand } =
+    req.body
+  // Create a combined query object
+  let filterQuery = {}
+  // Add search text to query object
+  if (query) {
+    filterQuery = {
+      ...filterQuery,
+      $or: [
+        { title: { $regex: query, $options: 'i' } },
+        { description: { $regex: query, $options: 'i' } },
+      ],
+    }
+  }
+  // Add min and max price to query object
+  if (price !== undefined) {
+    filterQuery = {
+      ...filterQuery,
       price: {
         $gte: price[0],
         $lte: price[1],
       },
-    })
-      .populate('category', '_id name')
-      .populate('subcategories', '_id name')
-      .populate('postedBy', '_id name')
-      .exec()
-
-    res.json(products)
-  } catch (error) {
-    console.log('PRICE FILTER BE ERROR -->', error)
+    }
   }
-}
-
-const handleCategory = async (req, res, category) => {
-  try {
-    const products = await Product.find({ category: category })
-      .populate('category', '_id name')
-      .populate('subcategories', '_id name')
-      .populate('postedBy', '_id name')
-      .exec()
-
-    res.json(products)
-  } catch (error) {
-    console.log('CATEGORY FILTER BE ERROR -->', error)
-  }
-}
-
-const handleStar = (req, res, stars) => {
-  // Create a new doc, based on the Project doc ($$ROOT) and add a new field (floorAverage) which is the average of ratings.star; find the docs that match the stars we get from fe; find the products in our db that matches the aggregates ids
-  Product.aggregate([
-    {
-      $project: {
-        document: '$$ROOT',
-        floorAverage: {
-          $floor: { $avg: '$ratings.star' },
-        },
-      },
-    },
-    {
-      $match: { floorAverage: stars },
-    },
-  ]).exec((error, aggregates) => {
-    if (error) console.log('AGGREGATE BE ERROR -->', error)
-    Product.find({ _id: aggregates })
-      .populate('category', '_id name')
-      .populate('subcategories', '_id name')
-      .populate('postedBy', '_id name')
-      .exec((error, products) => {
-        if (error) console.log('PRODUCT AGGREGATE BE ERROR -->', error)
-        res.json(products)
-      })
-  })
-}
-
-const handleSubcategory = async (req, res, subcategory) => {
-  const products = await Product.find({ subcategories: subcategory })
-    .populate('category', '_id name')
-    .populate('subcategories', '_id name')
-    .populate('postedBy', '_id name')
-    .exec()
-  res.json(products)
-}
-
-const handleShipping = async (req, res, shipping) => {
-  const products = await Product.find({ shipping: shipping })
-    .populate('category', '_id name')
-    .populate('subcategories', '_id name')
-    .populate('postedBy', '_id name')
-    .exec()
-  res.json(products)
-}
-
-const handleColor = async (req, res, color) => {
-  const products = await Product.find({ color: color })
-    .populate('category', '_id name')
-    .populate('subcategories', '_id name')
-    .populate('postedBy', '_id name')
-    .exec()
-  res.json(products)
-}
-
-const handleBrand = async (req, res, brand) => {
-  const products = await Product.find({ brand: brand })
-    .populate('category', '_id name')
-    .populate('subcategories', '_id name')
-    .populate('postedBy', '_id name')
-    .exec()
-  res.json(products)
-}
-
-exports.searchFilters = async (req, res) => {
-  const { query, price, category, stars, subcategory, shipping, color, brand } = req.body
-
-  if (query) {
-    console.log('QUERY FILTER BE -->', query)
-    await handleQuery(req, res, query)
-  }
-
-  if (price !== undefined) {
-    console.log('PRICE FILTER BE -->', price)
-    await handlePrice(req, res, price)
-  }
-
+  // Add category to query object
   if (category) {
-    console.log('CATEGORY FILTER BE -->', category)
-    await handleCategory(req, res, category)
+    filterQuery = {
+      ...filterQuery,
+      category: category,
+    }
   }
-
+  // Add rating to query object
   if (stars) {
-    console.log('STARS FILTER BE -->', stars)
-    await handleStar(req, res, stars)
+    filterQuery = {
+      ...filterQuery,
+      'ratings.star': stars,
+    }
   }
-
+  // Add subcategory to query object
   if (subcategory) {
-    console.log('SUBCATEGORY FILTER BE -->', subcategory)
-    await handleSubcategory(req, res, subcategory)
+    filterQuery = {
+      ...filterQuery,
+      subcategories: subcategory,
+    }
   }
-
+  // Add shipping to query object
   if (shipping) {
-    console.log('SHIPPING FILTER BE -->', shipping)
-    await handleShipping(req, res, shipping)
+    filterQuery = {
+      ...filterQuery,
+      shipping: shipping,
+    }
   }
-
+  // Add color to query object
   if (color) {
-    console.log('COLOR FILTER BE -->', color)
-    await handleColor(req, res, color)
+    filterQuery = {
+      ...filterQuery,
+      color: color,
+    }
   }
-
+  // Add brand to query object
   if (brand) {
-    console.log('BRAND FILTER BE -->', brand)
-    await handleBrand(req, res, brand)
+    filterQuery = {
+      ...filterQuery,
+      brand: brand,
+    }
+  }
+  // Find products in db based on filters and send to fe
+  try {
+    const products = await Product.find(filterQuery)
+      // .select('-ratings') // You may want to exclude certain fields like ratings from the response
+      .exec()
+    // Populate products specs
+    const populatedProducts = await applyPopulations(products)
+    // Send to fe
+    res.json(populatedProducts)
+  } catch (error) {
+    console.log('FILTER BE ERROR -->', error)
+    res.status(500).json({ error: 'Internal Server Error' })
   }
 }
