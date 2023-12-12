fd856f886f9c47905971797ef7f12e9bf3a619ba
eduard <avas.eduard@gmail.com>
2023-12-12 18:46:59 +0200

    moved fbserviceaccountkey to .env and modified index.js accordingly

--- firebase/index.js
+++ firebase/index.js
@@ -1,7 +1,23 @@
-// Initialize firebase admin tool
+// // OLD CODE
+// // Initialize firebase admin tool
+// var admin = require('firebase-admin')
+
+// var serviceAccount = require('../config/fbServiceAccountKey.json')
+
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount),
+// })
+
+// module.exports = admin
+
+// NEW CODE
+// Install dotenv by running: npm install dotenv
+require('dotenv').config()
+
 var admin = require('firebase-admin')
 
-var serviceAccount = require('../config/fbServiceAccountKey.json')
+// Access the FB_SERVICE_ACCOUNT_KEY from the environment variables
+const serviceAccount = JSON.parse(process.env.FB_SERVICE_ACCOUNT_KEY)
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
