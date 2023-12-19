ae8a40569c4552be6f0d5f11cb63f957122a031a
eduard <avas.eduard@gmail.com>
2023-12-19 08:15:36 +0200

    modified cors setting for render deployment

--- server.js
+++ server.js
@@ -23,7 +23,41 @@ mongoose
 // middlewares
 app.use(morgan('dev'))
 app.use(bodyParser.json({ limit: '2mb' }))
-app.use(cors())
+// app.use(cors())
+
+// Set middleware of CORS
+app.use((req, res, next) => {
+  res.setHeader(
+    'Access-Control-Allow-Origin',
+    'https://ishop-frontend.onrender.com/'
+  )
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE'
+  )
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers'
+  )
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Private-Network', true)
+  res.setHeader('Access-Control-Max-Age', 7200)
+  next()
+})
+// Set preflight
+app.options('*', (req, res) => {
+  console.log('preflight')
+  if (
+    req.headers.origin === 'https://ishop-frontend.onrender.com/' &&
+    allowMethods.includes(req.headers['access-control-request-method']) &&
+    allowHeaders.includes(req.headers['access-control-request-headers'])
+  ) {
+    console.log('pass')
+    return res.status(204).send()
+  } else {
+    console.log('fail')
+  }
+})
 
 // middleware routes
 readdirSync('./routes/').map(route =>
