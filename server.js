ff2ad9eb9ccedf09f7eabd36204607c4d44fda95
eduard <avas.eduard@gmail.com>
2023-12-21 08:17:48 +0200

    added cors option object for render.com domain

--- server.js
+++ server.js
@@ -1,9 +1,77 @@
+// const express = require('express')
+// const mongoose = require('mongoose')
+// const morgan = require('morgan')
+// const bodyParser = require('body-parser')
+// const cors = require('cors')
+// const { readdirSync } = require('fs') // node js file system
+// require('dotenv').config()
+
+// // app
+// const app = express()
+
+// // db connection
+// mongoose
+//   .connect(process.env.DATABASE, {
+//     useNewUrlParser: true,
+//     useCreateIndex: true,
+//     useFindAndModify: false,
+//     useUnifiedTopology: true,
+//   })
+//   .then(() => console.log('DATABASE CONNECTION SUCCES'))
+//   .catch(error => console.log(`DATABASE CONNECTION ERROR: ${error}`))
+
+// // middlewares
+// app.use(morgan('dev'))
+// app.use(bodyParser.json({ limit: '2mb' }))
+// app.use(cors())
+
+// // // Set middleware of CORS
+// // app.use((req, res, next) => {
+// //   res.setHeader('Access-Control-Allow-Origin', '*')
+// //   res.setHeader(
+// //     'Access-Control-Allow-Methods',
+// //     'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE'
+// //   )
+// //   res.setHeader(
+// //     'Access-Control-Allow-Headers',
+// //     'Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers'
+// //   )
+// //   res.setHeader('Access-Control-Allow-Credentials', true)
+// //   res.setHeader('Access-Control-Allow-Private-Network', true)
+// //   res.setHeader('Access-Control-Max-Age', 7200)
+// //   next()
+// // })
+// // // Set preflight
+// // app.options('*', (req, res) => {
+// //   console.log('preflight')
+// //   if (
+// //     req.headers.origin === 'http://localhost:8000/' &&
+// //     allowMethods.includes(req.headers['access-control-request-method']) &&
+// //     allowHeaders.includes(req.headers['access-control-request-headers'])
+// //   ) {
+// //     console.log('pass')
+// //     return res.status(204).send()
+// //   } else {
+// //     console.log('fail')
+// //   }
+// // })
+
+// // middleware routes
+// readdirSync('./routes/').map(route =>
+//   app.use('/api', require('./routes/' + route))
+// ) // use all routes from the routes directory
+
+// //  port
+// const port = process.env.PORT || 8000
+
+// app.listen(port, () => console.log(`Server is running on port ${port}`))
+
 const express = require('express')
 const mongoose = require('mongoose')
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
 const cors = require('cors')
-const { readdirSync } = require('fs') // node js file system
+const { readdirSync } = require('fs') // Node.js file system
 require('dotenv').config()
 
 // app
@@ -17,51 +85,26 @@ mongoose
     useFindAndModify: false,
     useUnifiedTopology: true,
   })
-  .then(() => console.log('DATABASE CONNECTION SUCCES'))
+  .then(() => console.log('DATABASE CONNECTION SUCCESS'))
   .catch(error => console.log(`DATABASE CONNECTION ERROR: ${error}`))
 
 // middlewares
 app.use(morgan('dev'))
 app.use(bodyParser.json({ limit: '2mb' }))
-app.use(cors())
 
-// // Set middleware of CORS
-// app.use((req, res, next) => {
-//   res.setHeader('Access-Control-Allow-Origin', '*')
-//   res.setHeader(
-//     'Access-Control-Allow-Methods',
-//     'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE'
-//   )
-//   res.setHeader(
-//     'Access-Control-Allow-Headers',
-//     'Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers'
-//   )
-//   res.setHeader('Access-Control-Allow-Credentials', true)
-//   res.setHeader('Access-Control-Allow-Private-Network', true)
-//   res.setHeader('Access-Control-Max-Age', 7200)
-//   next()
-// })
-// // Set preflight
-// app.options('*', (req, res) => {
-//   console.log('preflight')
-//   if (
-//     req.headers.origin === 'http://localhost:8000/' &&
-//     allowMethods.includes(req.headers['access-control-request-method']) &&
-//     allowHeaders.includes(req.headers['access-control-request-headers'])
-//   ) {
-//     console.log('pass')
-//     return res.status(204).send()
-//   } else {
-//     console.log('fail')
-//   }
-// })
+// cors configuration
+const corsOptions = {
+  origin: 'https://ishop-frontend.onrender.com',
+  optionsSuccessStatus: 200, // for legacy browsers that choke on 204
+}
+app.use(cors(corsOptions))
 
 // middleware routes
 readdirSync('./routes/').map(route =>
   app.use('/api', require('./routes/' + route))
 ) // use all routes from the routes directory
 
-//  port
+// port
 const port = process.env.PORT || 8000
 
 app.listen(port, () => console.log(`Server is running on port ${port}`))
